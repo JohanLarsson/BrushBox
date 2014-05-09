aa7e0947bcd324b375a63e9474ed96f93d476094
Johan Larsson <johan_gavle1@hotmail.com>
2014-05-09 21:12:56 +0200

    Initial

---
+++ BrushBox/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace BrushBox
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Media;
+    using Annotations;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private SolidColorBrush _brush;
+        public ViewModel()
+        {
+            Brush = Brushes.HotPink;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SolidColorBrush Brush
+        {
+            get { return _brush; }
+            set
+            {
+                if (Equals(value, _brush))
+                {
+                    return;
+                }
+                _brush = value;
+                OnPropertyChanged();
+            }
+        }
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
